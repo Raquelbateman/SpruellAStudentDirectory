8fa7ae4ef0848b45acd0abff1ebaa4c7f395585c
Raquelbateman <aliciaraquel1017@gmail.com>
2024-07-25 21:13:04 -0700

    models updated

--- API/Models/Student.cs
+++ API/Models/Student.cs
@@ -7,9 +7,13 @@ namespace API.Models
 {
     public class Student
     {
-        public int StudentId { get; set;}
-        public string StudentName { get; set;} ="";
+         public int Id { get; set; }
+        public string? Name { get; set; }
 
-        public bool IsDeleted { get; set;}
+        public string? Address { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
     } 
 }
