b92cfa1b3b44e8c39b819fa08bfc7a506727d2c3
Raquelbateman <aliciaraquel1017@gmail.com>
2024-07-27 20:46:13 -0700

    push client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ffd8fea0bbddbcf223fb9d582b794706c1d726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41627c65deca653de013bfedd0e22787028b1ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
